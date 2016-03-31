9e9e7276d93b45c59c844c482e65557d7d8c5d28
matheusoc <matheuscampos@gec.inatel.br>
2016-03-31 12:50:04 -0300

    Atualização

--- app/src/main/java/br/com/getapp/structure/view/RemoveAlarmActivity.java
+++ app/src/main/java/br/com/getapp/structure/view/RemoveAlarmActivity.java
@@ -100,7 +100,7 @@ public class RemoveAlarmActivity extends AppCompatActivity {
                     refreshList();
                 }
             });
-            Log.i("muda","muda");
+
             return layout;
         }
     }
