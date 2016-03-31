804abf1ab931db971b72981c60ee6ddab98b681d
Matheus de Oliveira Campos <matheusoliveira@inatel.br>
2016-03-31 14:08:20 -0300

    Update

--- app/src/main/java/br/com/getapp/structure/view/MainActivity.java
+++ app/src/main/java/br/com/getapp/structure/view/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(new AlarmAdapter(this));
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/br/com/getapp/structure/view/RemoveAlarmActivity.java
+++ app/src/main/java/br/com/getapp/structure/view/RemoveAlarmActivity.java
@@ -1,13 +1,21 @@
 package br.com.getapp.structure.view;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import br.com.getapp.R;
-import br.com.getapp.structure.adapter.RemoveAlarmAdapter;
+import br.com.getapp.structure.SQL.DataBase;
+import br.com.getapp.structure.model.Alarm;
 
 /**
  * Created by User on 16/03/2016.
@@ -23,5 +31,58 @@ public class RemoveAlarmActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listView2);
         listView.setAdapter(new RemoveAlarmAdapter(this));
 
+    }
+
+    public void refreshList() {
+        listView.setAdapter(new RemoveAlarmAdapter(this));
+    }
+
+    public class RemoveAlarmAdapter extends BaseAdapter{
+
+        private Context context;
+        private ArrayList<Alarm> alarms;
+
+        RemoveAlarmAdapter(Context context) {
+            this.context = context;
+            DataBase db = new DataBase(context);
+            alarms = db.search();
+        }
+
+        @Override
+        public int getCount() {
+            return alarms.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return alarms.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final int pos = position;
+            final DataBase db = new DataBase(context);
+
+            final Alarm clock = alarms.get(position);
+
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.removeralarm_adapter_layout, null);
+
+            ImageButton imgBtn = (ImageButton) layout.findViewById(R.id.removeImageButton);
+            imgBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    db.delete(clock);
+                    refreshList();
+                }
+            });
+
+            return layout;
+        }
     }
 }
