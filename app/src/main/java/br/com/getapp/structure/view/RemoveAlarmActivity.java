c333ede3ef0d4e26b4f5fb942e1136d37a3ab851
matheusoc <matheuscampos@gec.inatel.br>
2016-03-31 12:46:52 -0300

    Atualização

--- app/src/main/java/br/com/getapp/structure/view/RemoveAlarmActivity.java
+++ app/src/main/java/br/com/getapp/structure/view/RemoveAlarmActivity.java
@@ -3,6 +3,7 @@ package br.com.getapp.structure.view;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -99,7 +100,7 @@ public class RemoveAlarmActivity extends AppCompatActivity {
                     refreshList();
                 }
             });
-
+            Log.i("muda","muda");
             return layout;
         }
     }
